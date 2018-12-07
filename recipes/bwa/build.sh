9626d051de67a1ac1143934119e7a3f1cd5b35f3
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2018-12-06 19:32:55 -0800

    Add patch for intel branch

--- recipes/bwa/build.sh
+++ recipes/bwa/build.sh
@@ -2,7 +2,7 @@
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export CXX_FLAGS="-std=c++11 -pthread -msse3"
+export CXX_FLAGS="-std=c++11 -pthread -msse4.1"
 make
 mkdir -p $PREFIX/bin
 cp bwa $PREFIX/bin
